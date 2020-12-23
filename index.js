a426facd07103894033526808a91f65267e3289b
Kayc <kaycmendes@gmail.com>
2020-12-23 19:05:56 -0300

    add adblock

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-import puppeteer from 'puppeteer';
+import puppeteer from 'puppeteer-extra';
+import AdblockerPlugin from 'puppeteer-extra-plugin-adblocker'
+puppeteer.use(AdblockerPlugin())
+
+
 export default class opgg_scraper {
     constructor(user) {
         this.user = user;
@@ -7,22 +11,22 @@ export default class opgg_scraper {
 
     async getStats(user, region, refresh) {
         try {
-            const browser = await puppeteer.launch();
+            const browser = await puppeteer.launch({ headless: false });
             const page = await browser.newPage();
             await page.goto(`https://${region}.op.gg/summoner/userName=${user}`)
-            await page.waitForSelector('div.GameItemList');
-            await page.waitForTimeout(1000);
-            await page.click('#right_gametype_soloranked');
-            await page.waitForTimeout(1000);
-            await page.waitForSelector('div.GameItemList');
-
             if (refresh === true) {
                 await page.click('#SummonerRefreshButton')
                 await page.waitForSelector('div.GameItemList')
+                await page.click('#right_gametype_soloranked');
             } else { }
+            await page.waitForSelector('div.GameItemList');
+            await page.click('#right_gametype_soloranked');
+            await page.waitForTimeout(2000);
+            await page.waitForSelector('div.GameItemList');
+            await page.waitForTimeout(1000);
+
             const fetchWins = await page.$$('div.Win');
             const fetchLoses = await page.$$('div.Lose');
-            const name = await page.$$eval('span.Name', e => e[1].innerText);
             const level = await page.$eval('.Level', e => e.innerText);
             const rank = await page.$eval('.TierRank', e => e.innerText);
             const pdl = await page.$eval('.LeaguePoints', e => e.innerText);
@@ -33,7 +37,7 @@ export default class opgg_scraper {
             const image = await page.$eval('img.ProfileImage', e => e.src);
 
             const stats = {
-                'Name': name,
+                'Name': user,
                 'Level': level,
                 'ProfilePic': image,
                 'LastMatches': fetchWins.length + fetchLoses.length,
@@ -49,7 +53,7 @@ export default class opgg_scraper {
             await browser.close();
             return stats
         } catch (e) {
-            return console.log(`Player not found. This player may not exist on the this region.
+            return console.log(`Player not found. This player may not exist on the this region. Or haven't played ranked yet.
             Ctrl+C to quit 
             `)
         }
